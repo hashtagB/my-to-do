6151dbaecc54364b987ebb02495d0f703d3c0ba2
bhargavimarudhuri <bhargavimarudhuri@gmail.com>
2021-03-22 21:15:49 -0400

    page structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+//importing components
+import Form from './components/Form';
+import ToDoList from './components/TodoList';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>My Todo List</h1>
+      </header>
+      <Form />
+      <ToDoList />
     </div>
   );
 }
