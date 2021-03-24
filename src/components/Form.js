93fa6efaa22af64846117ff07d6f6f025c028289
bhargavimarudhuri <bhargavimarudhuri@gmail.com>
2021-03-23 22:25:28 -0400

    worked on form submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,27 +2,25 @@ import React from 'react';
 
 
 const Form = ({setInputText, todos, setTodos, inputText}) => {
-
-    const inputTextHandler = (e) => {
-        
+    const inputTextHandler = (e) => {    
         console.log(e.target.value);
         setInputText(e.target.value);
     }
-    const submitToDoHandler = (e) => {
 
+    const submitToDoHandler = (e) => {
         e.preventDefault();
         setTodos([
             ...todos, {text: inputText, completed: false, id: Math.random() * 1000 }
-        ]
-
-        )
+            ])
+        setInputText("");
     };
+
     return(
         <form>
-                <input onChange={inputTextHandler} type="text" className="todo-input" />
-                    <button onClick={submitToDoHandler} className="todo-button" type="submit">
-                         <i className="fas fa-plus-square"></i>
-                    </button>
+            <input onChange={inputTextHandler} type="text" value={inputText} className="todo-input"/>
+            <button onClick={submitToDoHandler} className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
+            </button>
             <div className="select">
                  <select name="todos" className="filter-todo">
                     <option value="all">All</option>
@@ -32,6 +30,6 @@ const Form = ({setInputText, todos, setTodos, inputText}) => {
             </div>
         </form>
     );
-}
+} 
 
 export default Form;
